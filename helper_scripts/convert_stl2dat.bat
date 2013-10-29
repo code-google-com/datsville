a5f2c8a53365d376583f0cc69c4465704dfbe970
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2013-10-29 05:38:16 +0000

    Updated terrain models.

--- helper_scripts/convert_stl2dat.bat
+++ helper_scripts/convert_stl2dat.bat
@@ -1,2 +1,3 @@
-stl2dat meshlab_output_1.stl -ldr -raw
+::stl2dat "W:\LDraw\datsville\svn\trunk\terrain\worldmachine_output.stl" -ldr -raw
+stl2dat "W:\LDraw\datsville\svn\trunk\terrain\3dwin_output.stl" -ldr -raw
 pause
--- helper_scripts/terrain_colorer_execute.bat
+++ helper_scripts/terrain_colorer_execute.bat
@@ -1,2 +1,2 @@
-cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\terrain\meshlab_output_lines.ldr"
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\terrain\3dwin_output.ldr"
 pause
