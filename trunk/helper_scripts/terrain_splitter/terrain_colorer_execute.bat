8f52ce88d18d0784a81125c4712b3e2d75126cfb
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2013-11-07 22:40:40 +0000

    Finished writing "terrain_splitter.js". Moved everything to the correct folders. Copied the split terrain pieces to "datsville_terrain" folder.

---
+++ trunk/helper_scripts/terrain_splitter/terrain_colorer_execute.bat
@@ -0,0 +1,17 @@
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_01.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_02.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_03.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_04.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_05.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_06.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_07.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_08.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_09.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_10.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_11.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_12.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_13.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_14.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_15.ldr" -o
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_colorer\terrain_colorer.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup_16.ldr" -o
+pause
---
+++ trunk/helper_scripts/terrain_splitter/terrain_splitter_execute.bat
@@ -0,0 +1,3 @@
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_splitter.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_stepped_render_backup.ldr" -p 4
+::cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\terrain_splitter.js" "W:\LDraw\datsville\svn\trunk\helper_scripts\terrain_splitter\does_not_exist.ldr" -p 4
+pause
