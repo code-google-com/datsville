ce287b2e6a086ef18ef947ea3b53609d4c2956ae
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2013-12-03 06:55:16 +0000

    1. Some new buildings. (The wiki is out of date unfortunately.) 2. Several re-arranged models. (I'm not too happy about this, but whatever.) 3. A new "datsville_boxed" folder featuring a polygon reduced version of Datsville. 4. Some new mostly empty blocks on the West side of town.

---
+++ trunk/helper_scripts/myboxer/myboxer_convert_datsville_boxed.bat
@@ -0,0 +1,2 @@
+cscript "W:\LDraw\datsville\svn\trunk\helper_scripts\myboxer\myboxer.js" "W:\LDraw\datsville\svn\trunk\datsville_boxed" -o
+pause
--- trunk/helper_scripts/povray/copy_this.pov
+++
@@ -1,2 +0,0 @@
-#include "ldr_wrapper_1.4.inc"
-#include "ldr_wrapper_showcase_1.4.inc"
