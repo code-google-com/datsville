1c61494772451d38465dd1499772791990d82de3
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2013-12-09 11:07:03 +0000

    1. Update town map. 2. Added two new vehicles by Tim Gould. 3. Minor tweaks to some other models.

--- trunk/helper_scripts/povray/l3p_convert_datsville_boxed.cmd
+++ trunk/helper_scripts/povray/l3p_convert_datsville_boxed.cmd
@@ -1,2 +1,2 @@
-"C:\Programs Windows\l3p14beta\l3p.exe" "W:\LDraw\datsville\svn\trunk\datsville_boxed\datsville.ldr" -o -il -ic
+"C:\Programs Windows\l3p14beta\l3p.exe" "W:\LDraw\datsville\svn\trunk\datsville_boxed\datsville.ldr" -o -il -ic > l3p_errors.txt
 pause
