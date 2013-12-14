81b592a6b97fc4acf1cfbb7ebc5d507055a650b4
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2013-12-14 03:35:11 +0000

    1. Messing with POVray stuff.

--- helper_scripts/povray/l3p_convert_building_053_animalexhibit.cmd
+++ helper_scripts/povray/l3p_convert_building_053_animalexhibit.cmd
@@ -1,2 +1,2 @@
-"C:\Programs Windows\l3p14beta\l3p.exe" "W:\LDraw\datsville\google_code_rev004\building_053_animalexhibit.ldr" -o -cg30,45,0
+"C:\Programs Windows\l3p14beta\l3p.exe" "W:\LDraw\datsville\svn\trunk\datsville\building_053_animalexhibit.ldr" -o -cg30,45,0
 pause
