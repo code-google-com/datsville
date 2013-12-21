a197d15dc799ac122d7dae636851259bf81288a0
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2013-12-21 13:58:22 +0000

    1. Made the airport larger. 2. Added another control tower to the airport. 3. Added the police graduation ceremony to the cricket grounds. 4. Rail is now double wide for its entire length. 5. Fixed some minor issues with streets. 6. Added fire hydrants to most city blocks. 7. Fixed issues with motorcycles not showing up.

--- trunk/helper_scripts/LDView_SaveSnapshot_x64_boxed.bat
+++ trunk/helper_scripts/LDView_SaveSnapshot_x64_boxed.bat
@@ -1,4 +1,4 @@
 set LDVIEW_EXE="C:\Program Files\LDView\LDView64.exe"
-cd "W:\LDraw\datsville\svn\trunk\datsville"
+cd "W:\LDraw\datsville\svn\trunk\datsville_boxed"
 for %%a in ( "*.dat", "*.ldr", "*.mpd" ) do %LDVIEW_EXE% "%%a" -SaveSnapshot=%%~na.png -PreferenceSet=DatsvilleSVN -SaveActualSize=0 -SaveImageType=1 -SaveZoomToFit=1 -SaveWidth=400 -SaveHeight=300 -DefaultZoom=0.95 -FOV=10 -DefaultLatLong=30,45
 pause
