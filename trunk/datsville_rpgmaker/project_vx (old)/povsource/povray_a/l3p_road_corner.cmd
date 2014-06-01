0586ae0e0c50068981e22fcdac9f94c9dbcc88e8
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2014-06-01 19:27:15 +0000

    Update RPG Maker stuff.

---
+++ trunk/datsville_rpgmaker/project_vx (old)/povsource/povray_a/l3p_road_corner.cmd
@@ -0,0 +1,2 @@
+"C:\Programs Windows\l3p14beta\l3p.exe" "W:\JA2\mecha\povray\road_corner.ldr" -o -ic -il -ibmecha.inc
+pause
---
+++ trunk/datsville_rpgmaker/project_vx (old)/povsource/povray_b/block_020_buildings.cmd
@@ -0,0 +1,2 @@
+"C:\Programs Windows\l3p14beta\l3p.exe" "W:\mecha\povray_b\block_020_buildings.ldr" -o -ic -il -ibmecha.inc
+pause
