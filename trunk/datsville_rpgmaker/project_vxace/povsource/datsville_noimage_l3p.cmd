32127f9770e22ab90869d36cf90aa17e4a948328
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2014-06-17 07:31:52 +0000

    1. Tweaks to street layer decorations. 2. Rename/organize some infrastructure items. 3. Update RPG to reflect these changes.

--- trunk/datsville_rpgmaker/project_vxace/povsource/datsville_noimage_l3p.cmd
+++ trunk/datsville_rpgmaker/project_vxace/povsource/datsville_noimage_l3p.cmd
@@ -1,2 +1,2 @@
-"E:\Programs Windows\l3p14beta\l3p.exe" "D:\Datsville\trunk\datsville_rpgmaker\project_vxace_temp01\povsource\datsville_noimage.ldr" -o -ic -il -ibbuilding_mapsize.inc
+"E:\Programs Windows\l3p14beta\l3p.exe" "D:\Datsville\trunk\datsville_rpgmaker\project_vxace\povsource\datsville_noimage.ldr" -o -ic -il -ibbuilding_mapsize.inc
 pause
