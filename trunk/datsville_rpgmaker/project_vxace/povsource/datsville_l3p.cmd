e20d6618711a798d7e08dddfd5df723f625e3e9a
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2014-06-26 21:32:36 +0000

    Shadows are now working for the overlay layer and character sprites in RPG.

---
+++ trunk/datsville_rpgmaker/project_vxace/povsource/datsville_l3p.cmd
@@ -0,0 +1,2 @@
+"E:\Programs Windows\l3p14beta\l3p.exe" "datsville.ldr" -o -ic -il -ibbuilding_mapsize.inc
+pause
--- trunk/datsville_rpgmaker/project_vxace/povsource/datsville_noimage_l3p.cmd
+++
@@ -1,2 +0,0 @@
-"E:\Programs Windows\l3p14beta\l3p.exe" "D:\Datsville\trunk\datsville_boxed\datsville_noimage.ldr" -o -ic -il -ibbuilding_mapsize.inc
-pause
