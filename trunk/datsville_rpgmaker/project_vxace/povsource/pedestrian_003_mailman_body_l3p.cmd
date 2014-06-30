4d0a369077a537a9be9e12ef3ab6fc45902f8c71
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2014-06-30 22:41:39 +0000

    1. Added a new building interior and character sprite to the RPG. 2. In the RPG you now have to press the action button to interact with NPCs. 3. Started a new "furniture" sub-model category.

---
+++ trunk/datsville_rpgmaker/project_vxace/povsource/pedestrian_003_mailman_body_l3p.cmd
@@ -0,0 +1,2 @@
+"E:\Programs Windows\l3p14beta\l3p.exe" "D:\Datsville\trunk\datsville_rpgmaker\project_vxace\povsource\pedestrian_003_mailman.ldr" "D:\Datsville\trunk\datsville_rpgmaker\project_vxace\povsource\pedestrian_003_mailman_body.pov" -o -ic -il -ibcharacter_body.inc
+pause
