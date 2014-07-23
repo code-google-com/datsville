0a67028d6b5aa270c68bb59450e9be8cc22af817
mikh2161 <mikh2161@b20d4e50-d6a4-11de-bdb9-4bb2e43cceb2>
2014-07-23 03:23:26 +0000

    Datsville RPG has moved to its own repository.

--- datsville_rpgmaker/pov_source/pedestrian_003_mailman_body_l3p.cmd
+++
@@ -1,2 +0,0 @@
-"E:\Programs Windows\l3p14beta\l3p.exe" "D:\Datsville\trunk\datsville_rpgmaker\pov_source\pedestrian_003_mailman.ldr" "D:\Datsville\trunk\datsville_rpgmaker\pov_source\pedestrian_003_mailman_body.pov" -o -ic -il -ibcharacter_body.inc
-pause
--- datsville_rpgmaker/pov_source/pov_color_tests/glass_test_case_l3p.cmd
+++
@@ -1,2 +0,0 @@
-"E:\Programs Windows\l3p14beta\l3p.exe" "D:\Datsville\trunk\datsville_rpgmaker\pov_source\glass_test_case.ldr" -o
-pause
